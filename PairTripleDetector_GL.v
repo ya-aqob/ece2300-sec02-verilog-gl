bef185735b2a3e2a280f24963a76df497b6ea3b1
jmf445 <jmf445@cornell.edu>
2025-09-05 14:02:20 -0400

    suboptimal implementation

--- PairTripleDetector_GL.v
+++ PairTripleDetector_GL.v
@@ -13,22 +13,28 @@ module PairTripleDetector_GL
   input  wire in1,
   input  wire in2,
   output wire out
- );
+);
 
-  //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  // Implement pair/triple detector using explicit gate-level modeling
-  //''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // NOT gates to create complement of each input
 
-  wire w;
-  or(w, in0, in1);
+  wire not_in0, not_in1, not_in2;
 
-  wire y;
-  and(y, w, in2);
+  not( not_in0, in0 );
+  not( not_in1, in1 );
+  not( not_in2, in2 );
 
-  wire x;
-  and(x, in0, in1);
+  // AND gates for each row in the truth table where output is one
 
-  or(out, y, x);
+  wire row3, row5, row6, row7;
+
+  and( row3, not_in0, in1,     in2     );
+  and( row5, in0,     not_in1, in2     );
+  and( row6, in0,     in1,     not_in2 );
+  and( row7, in0,     in1,     in2     );
+
+  // OR together the outputs of the AND gates
+
+  or( out, row3, row5, row6, row7 );
 
 endmodule
 
